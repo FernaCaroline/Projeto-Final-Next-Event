209d41dbbf8e57a690193176e90fe86e2be523e6
Anna Gagelinski <anna.gagelinski@gmail.com>
2025-10-16 16:48:45 -0300

    Versão com o Entity Framework ajustado (ajuste 9.0.10 → 8.0.20)

--- NextEvent/NextEvent/Migrations/20251016042813_InitialClean.Designer.cs
+++ NextEvent/NextEvent/Migrations/20251016042813_InitialClean.Designer.cs
@@ -18,7 +18,7 @@ namespace NextEvent.Migrations
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
+            modelBuilder.HasAnnotation("ProductVersion", "8.0.20");
 
             modelBuilder.Entity("NextEvent.Models.Administrador", b =>
                 {
